6934bea719a2e775cc40f9237db56ca909dd04b2
ppszczepaniak <p.p.szczepaniak@gmail.com>
2019-10-06 09:32:57 +0200

    initial commit

---
+++ src/main/java/com/example/BookOrderSpringApp/BookOrderSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookOrderSpringApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookOrderSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookOrderSpringAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BookOrderSpringApp/BookOrderSpringAppApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.BookOrderSpringApp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BookOrderSpringAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
